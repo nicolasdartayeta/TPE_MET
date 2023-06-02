a482372e35219a676d8cb9fab5ffd66469fdb09c
Federico Garate <pipigarate@gmail.com>
2023-06-02 20:34:07 -0300

    Prueba Usuario

--- Plataforma/src/Usuario.java
+++ Plataforma/src/Usuario.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Usuario {
+package PACKAGE_NAME;
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String claveAcceso;
+    private int dni;
+    public Usuario(){
+
+    }
+    public String getNombre():
+    public String getApellido() {
+
+    }
+    public String getClaveAcceso(){
+
+    }
+    public int getDni(){
+
+    }
+    public String setNombre(){
+
+    }
+    public String setClaveAcceso(){
+
+    }
+    public String setApellido(){
+    }
+    public int setDni{
+
+    }
 }
