8abeb1467b0bc4d66f7019f2ab30216a05662d99
Federico Garate <pipigarate@gmail.com>
2023-06-02 21:46:37 -0300

    Atributo y metodos de pasajero agregados

---
+++ Plataforma/src/Pasajero.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Pasajero extends Usuario{
+    private ArrayList<TarjetaCredito> tarjetas;
+    private ArrayList<Compra> compras;
+    private int creditoCompra;
+
+    public Pasajero(String nombre, String apellido, String claveAcceso, int dni){
+
+    }
+    public ArrayList<TarjetaCredito> getTarjetas(){
+    }
+
+    public void addTarjeta(TarjetaCredito tc){
+
+    }
+    public ArrayList<Compra> getCompras(){
+
+    }
+    public void addCompra(Compra c){
+
+    }
+
+}
