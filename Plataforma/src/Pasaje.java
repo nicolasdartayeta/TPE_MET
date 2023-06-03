92686f5cafae40384c6ae10b8fa9885d00bd7c3e
estebanferz <fernandezesteban344@gmail.com>
2023-06-03 16:46:58 -0300

    Pasaje

--- Plataforma/src/Pasaje.java
+++ Plataforma/src/Pasaje.java
@@ -1,3 +1,23 @@
 public class Pasaje {
-    // test
+
+    private int numeroAsiento;
+    private Pasajero pasajero;
+    private Servicio servicio;
+    private int precio;
+
+    public int getNumeroAsiento() {
+        return numeroAsiento;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
 }
