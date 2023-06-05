a46f211a572c90936263773c5da77778a705607c
Nicolas <nicodarta1305@gmail.com>
2023-06-05 18:55:39 -0300

    se muestran los creditos antes de seleccionar metodo de pago. otras mejoras.

--- Plataforma/src/Main.java
+++ Plataforma/src/Main.java
@@ -36,11 +36,28 @@ public class Main {
                 case 1:
                     if (!listaSeleccionados.isEmpty()) {
                         System.out.println("Pagar con tarjeta [0] | Pagar con creditos [1]");        //COMPRAR PASAJE
+                        System.out.println("El usuario tiene: " + pasajero1.getCreditoCompra() + " creditos.");
                         System.out.println("El usuario ya tiene tarjetas precargadas"); // TODO implementar funciones del pasajero
                         input = scanner.nextInt();
 
                         if (input == 0) {
-                            pasajero1.addCompra(plataforma.crearCompra(pasajero1, listaSeleccionados, pasajero1.elegirTarjeta()));
+                            System.out.println("Pagar con tarjeta guardada [0] | Agregar nueva tarjeta [1]");        //COMPRAR PASAJE
+                            input = scanner.nextInt();
+
+                            if(input == 0) {
+                                pasajero1.addCompra(plataforma.crearCompra(pasajero1, listaSeleccionados, pasajero1.elegirTarjeta()));
+                            } else if(input == 1){
+                                System.out.println("Ingrese nro de tarjeta: ");
+                                long nroTarjeta = scanner.nextLong();
+                                System.out.println("Ingrese banco: ");
+                                scanner = new Scanner(System.in);
+                                String banco = scanner.nextLine();
+                                System.out.println("Ingrese marca:");
+                                String marca = scanner.nextLine();
+                                TarjetaCredito tarj = new TarjetaCredito(nroTarjeta, banco, marca);
+
+                                pasajero1.addCompra(plataforma.crearCompra(pasajero1, listaSeleccionados, tarj));
+                            }
                         } else if (input == 1) {
                             pasajero1.addCompra(plataforma.crearCompra(pasajero1, listaSeleccionados));
                         };
@@ -53,7 +70,7 @@ public class Main {
                     if (aux != null){
                         aux.imprimirResumen(); // SE IMPRIME EL RESUMEN DE LA COMPRA QUE ELIGIO EL PASAJERO
 
-                        if (!aux.getEstado()) {
+                        if (!aux.getEstado()) { // No esta pagada
                             System.out.println("Desea confirmar la compra? y or n");
                             Scanner scanner2 = new Scanner(System.in);
                             String inputS = scanner2.nextLine();
@@ -61,13 +78,14 @@ public class Main {
                             if (inputS.equals("y")) {
                                 if (aux.getTarjetaDeCredito() == null) {
                                     aux.pagarCredito();
-                                    if (aux.getEstado()) {
-                                        System.out.println("Compra pagada");
-                                    } else {
-                                        System.out.println("No se realizo el pago");
-                                    }
                                 } else {
                                     aux.pagarTarjeta(pagosVisa);
+                                }
+
+                                if (aux.getEstado()) {
+                                    System.out.println("Compra pagada");
+                                } else {
+                                    System.out.println("No se realizo el pago");
                                 }
                             }
                         }
