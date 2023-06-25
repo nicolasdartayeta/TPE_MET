5545a27749134a57343cfde6f2977d5fa2483d23
Nicolas <nicodarta1305@gmail.com>
2023-06-25 17:39:51 -0300

    Cosas del login agregadas a plataforma

--- Plataforma/src/Plataforma.java
+++ Plataforma/src/Plataforma.java
@@ -8,51 +8,71 @@ public class Plataforma {
         usuarios = new ArrayList<Usuario>();
     }
 
-    public boolean login(){
+    public Usuario login(){
         System.out.println("LOGEARSE");
-        System.out.print("Ingresar nombre: ");
+        System.out.print("Ingresar DNI: ");
         Scanner scanner = new Scanner(System.in);
-        String inputNombre = scanner.nextLine();
+        int inputDNI = Integer.parseInt(scanner.nextLine());
         System.out.print("Ingresar contrasenia: ");
         String inputContrasenia = scanner.nextLine();
 
-        if (existeUsuario(inputNombre, inputContrasenia)){
-            System.out.println("INGRESADO");
-            return true;
-        }else{
-            System.out.println(" datos erroneos");
-            return false;
+        Usuario logeado = existeUsuario(inputDNI);
+
+        if (logeado != null){
+            boolean datosCorrectos = chequearContrasenia(inputDNI, inputContrasenia);
+            if (datosCorrectos)
+                System.out.println("Logueado");
+            else
+                System.out.println("Contrasenia incorrecta");
+        } else {
+            System.out.println("No existe un usuario con ese DNI");
+
         }
+        return logeado;
+
+    }
+
+    private boolean chequearContrasenia(int inputDNI, String inputContrasenia){
+        for (Usuario u: usuarios){
+            if (u.getDni() == inputDNI)
+                if (u.getClaveAcceso() == inputContrasenia)
+                    return true;
+                else
+                    return false;
+        }
+
+        return false;
     }
     public boolean registrarse(){
         System.out.println("Registrarse");
-        System.out.print("Ingresar nombre: ");
+        System.out.print("Ingresar DNI: ");
         Scanner scanner = new Scanner(System.in);
-        String inputNombre = scanner.nextLine();
-        System.out.print("Ingresar contrasenia: ");
-        String inputContrasenia = scanner.nextLine();
+        int inputDNI = scanner.nextInt();
 
-        if (existeUsuario(inputNombre, inputContrasenia)){
-            System.out.println("YA EXISTE");
-            return false;
-        }else{
+        if (existeUsuario(inputDNI) == null){
+            System.out.print("Ingresar nombre: ");
+            String inputNombre = scanner.nextLine();
+            System.out.print("Ingresar contrasenia: ");
+            String inputContrasenia = scanner.nextLine();
             System.out.print("Ingresar apellido: ");
             String inputApellido = scanner.nextLine();
-            System.out.print("Ingresar DNI: ");
-            int inputDNI = scanner.nextInt();
             Usuario aux = new Usuario(inputNombre, inputApellido, inputContrasenia, inputDNI);
             usuarios.add(aux);
             return true;
+
+        } else {
+            System.out.println("Ya existe un usuario con ese DNI");
+            return false;
         }
     }
 
-    public boolean existeUsuario(String nombre, String contrasenia){
-        for(int i = 0; i < usuarios.size(); i++){
-            if (usuarios.get(i).getNombre().equals(nombre) && usuarios.get(i).getClaveAcceso().equals(contrasenia)){
-                return true;
-            }
+    public Usuario existeUsuario(int dni){
+        for (Usuario u: usuarios){
+            if (u.getDni() == dni)
+                return u;
         }
-        return false;
+
+        return null;
     }
 
     public Compra crearCompra(Pasajero comprador, ArrayList<Pasaje> pasajes, TarjetaCredito tarjeta){ //Se crea una compra utilizando una tarjeta de credito del comprador
@@ -65,4 +85,19 @@ public class Plataforma {
         return c1;
     }
 
+    private boolean addUsuario(Usuario usuario){
+        int dniUsuario = usuario.getDni();
+        boolean yaEstaReg = false;
+
+        for (Usuario u: usuarios){
+            if (u.getDni() == dniUsuario)
+                yaEstaReg = true;
+        }
+
+        if (!yaEstaReg) {
+            usuarios.add(usuario);
+            return true;
+        } else
+            return false;
+    }
 }
